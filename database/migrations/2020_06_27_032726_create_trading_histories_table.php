d6a7a64a133138bba8992f8f9c5b4e4b22fcad47
motoshifurugen <amanogawa_m@icloud.com>
2020-06-28 07:58:53 +0900

    マイグレーションファイル作成

--- database/migrations/2020_06_27_032726_create_trading_histories_table.php
+++ database/migrations/2020_06_27_032726_create_trading_histories_table.php
@@ -14,7 +14,18 @@ class CreateTradingHistoriesTable extends Migration
     public function up()
     {
         Schema::create('trading_histories', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->dateTime('entry_dateTime');
+            $table->dateTime('exit_dateTime');
+            $table->string('symbol');
+            // enumは複数の変数に定数を割り当てる
+            $table->enum('order_type', ['BUY', 'SELL']);
+            $table->integer('profit');
+            // double('カラム名', '合計桁数', '小数点以下の桁数')
+            $table->double('amount', 10, 3);
+            $table->double('entry', 13, 5);
+            $table->double('exit', 13, 5);
+            $table->text('memo')->nullable(true);
             $table->timestamps();
         });
     }
