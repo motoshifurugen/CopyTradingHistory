b0cb61a9cd6ace643d7135620585608c950769bd
motoshifurugen <amanogawa_m@icloud.com>
2020-06-28 11:22:42 +0900

    コントローラー、ビュー作成

---
+++ app/Http/Controllers/TradingHistoryController.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\TradingHIstory;
+use Illuminate\Support\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\Rule;
+
+date_default_timezone_set('Asia/Tokyo');
+
+class TradingHistoryController extends Controller
+{
+    protected $ORDER_TYPES = ['BUY' => '買', 'SELL' => '売'];
+    protected $SYMBOLS = ['USDJPY' => 'USDJPY', 'N225_CFD', '日経225(CFD)', 'N225m' =>'日経225ミニ', 'BTC' => 'ビットコイン', 'DJI_CFD' => 'ダウ(CFD)', 'EURUSD' => 'EURUSD', 'OIL' => '原油', 'GBPJFY' => 'GBPJFY', 'AUDUSD', 'AUDUSD', 'GBPUSD' => 'GBPUSD'];
+
+    public function index() {
+        $trading_histories = $this->getTradingHIstories();
+        $performance = $this->calcPerformance($trading_histories);
+
+        $order_types = ['ALL' => '全て'] + $this->ORDER_TYPES;
+        return view('trading-histories.index', compact('trading_histories'), ['pf' => $performance, 'symbols' =>$this->SYMBOLS, 'orderTypes' =>$order_types]);
+    }
+
+    public function create() {
+        $th = new TradingHistory();
+        $th->exit = 0;
+        $th->entry = 0;
+        $th->amount = 1;
+        $th->profit = 0;
+        $th->exit_dateTime = now();
+        $c = new Carbon();
+        $th->entry_dateTime = $c->subSecond(60);
+        $th->symbol = 'USDJPY';
+        $th->order_type = 'BUY';
+
+        return $this->createViewWithReturn($th, 'create');
+    }
+
+    public function edit(TradingHistory $trading_history) {
+        return $this->createViewWithReturn($trading_history, 'edit');
+    }
+
+    public function store() {
+        $this->save();
+        return redirect()->to('trading-history');
+    }
+
+    public function update(TradingHistory $trading_history) {
+        $this->save($trading_history);
+        return redirect()->to('trading_history');
+    }
+
+    public function destroy(TradingHistory $trading_history) {
+        $trading_history->delete();
+        return redirect()->to('trading-history');
+    }
+
+    function createViewWithReturn($th, $type) {
+        $th->exit_dateTime = Carbon::parse($th->exit_dateTime)->format('Y-m-d\TH:i');
+        $th->entry_dateTime = Carbon::parse($th->entry_dateTime)->format('Y-m-d\TH:i');
+        return view('trading-histories.save', compact('th'), ['symbols'=>$this->SYMBLES, 'orderTypes'=>$this->ORDER_TYPES, 'type'=>$type]);
+    }
+
+    function save($data = null) {
+        request()->validate(['entty_dateTime' => 'before:exit_dateTime'])
+
+        if ($data ==== null) {
+            $data = new TradingHIstory();
+        }
+
+        $data->memo = request('memo');
+        $data->exit = request('exit');
+        $data->entry = request('entry');
+        $data->symbol = request('symbol');
+        $data->amount = request('amount');
+        $data->profit = request('profit');
+        $data->order_type = request('order_type');
+        $data->exit_dateTime = request('exit_dateTime');
+        $data->entry_dateTime = request('entry_dateTime');
+        $data->save();
+    }
+
+    function getTradingHistories() {
+        $query = DB::table('trading_histories');
+
+        if (request('symbol')) {
+            $query->whereIn('symbol', request('symbol'));
+        }
+
+        if(array_key_exists(request('order_type'), $this->ORDER_TYPES)) {
+            $query->where('order_type', request('order_type'));
+        }
+
+        if (request('date_from') && !request('date_to')) {
+            $query->where('exit_dateTime', '>=', request('date_from'));
+        } else if (!request('date_from') && request('date_to')) {
+            $query->where('exit_dateTime', '<=', request('date_to'));
+        } else if (request('date_from') && request('date_to')) {
+            $date_to = request('date_to');
+            $query->whereBetween('exit_dateTime', [request('date_from'), $date_to]);
+        }
+
+        return $query->orderBy('exit_dateTime', 'desc')->get();
+    }
+
+    function calcPerformance($trading_histories) {
+        $ret = [
+            'total_count' => 0,
+            'total_profit' => 0,
+            'win_count' => 0,
+            'loss_count' => 0,
+            'draw_count' => 0,
+            'win_profit' => 0,
+            'loss_profit' => 0,
+            'win_rate' => 0,
+            'profit_factor' => 0,
+            'average_win_profit' => 0,
+            'average_loss_profit' => 0
+        ];
+
+        foreach ($trading_histories as $th) {
+            if($th->profit < 0) {
+                $ret['loss_count']++;
+                $ret['loss_profit'] += $th->profit;
+            } else if ($th->profit > 0) {
+                $ret['win_count']++;
+                $ret['win_profit'] += $th->profit;
+            } else {
+                $ret['draw_count']++;
+            }
+            $ret['total_count']++;
+            $ret['total_profit'] += $th->profit;
+        }
+
+        $win_loss_count = $ret['win_count'] + $ret['loss_count'];
+
+        if (0 < $win_loss_count) {
+            $ret['win_rate'] = round($ret['win_count'] / $win_loss_count *100);
+        }
+
+        if(0 > $ret['loss_profit']) {
+            $ret['profit_factor'] = round($ret['win_profit'] / abs($ret['loss_profit']), 2);
+        }
+
+        if (0 < $ret['win_count']) {
+            $ret['average_win_profit'] = round($ret['win_profit'] / $ret['win_count']);
+        }
+
+        if (0 < $ret['loss_count']) {
+            $ret['average_loss_profit'] = round($ret['loss_profit'] / $ret['loss_count']);
+        }
+        return $ret;
+    }
+}
---
+++ resources/views/trading-histories/index.blade.php
@@ -0,0 +1,122 @@
+<style>
+* {
+    font-size: 0.9rem;
+}
+h4 {
+    padding: 0.25em 0.5em;
+    border-left: solid 5px blue;
+}
+th {
+    background-color:gray;
+    color: white;
+    font-weight: normal;
+}
+</style>
+
+<form action="/trading-history">
+    <table border="1" cellspacing="0" cellpadding="5">
+        <tr align="left">
+            <th>期間</th>
+            <td>
+                <input type="date" name="date_from" value=@if (request('date_from')) {{ request('date_from') }} @endif/>
+                &nbsp~$nbsp
+                <input type="date" name="date_to" value=@if(request('date_to')) {{ request('date_to') }} @endif/>
+            </td>
+        </tr>
+        <tr align="left">
+            <th>銘柄</th>
+            <td>
+                @foreach ($symbols as $key => $val)
+                    <input type="checkbox" name="symbol[]" value="{{ $key }}" / @if (request('symbol') && in_array($key, request('symbol'))) checked @endif>{{ $val }}
+                @endforeach
+            </td>
+        </tr>
+        <tr align="left">
+            <th>注文タイプ</th>
+            <td>
+                @foreach ($orderTypes as $key => $val)
+                    <input type="radio" name="order_type" value="{{ $key }}" @if (!request('order_type') && $key == 'ALL' || request('order_type') == $key) checked @endif /> {{ $val }}
+                @endforeach
+            </td>
+        </tr>
+        <tr align="center">
+            <td colspan="2">
+                <button type="submit">検索</button>
+                <button type="button" onclick="location.href='/trading-history'">リセット</button>
+            </td>
+        </tr>
+    </table>
+</form>
+
+<h4>パフォーマンス</h4>
+<table border="1" cellspacing="0" cellpadding="5">
+    <tr align="center">
+        <th>総回数</th>
+        <th>総損益</th>
+        <th>総利益</th>
+        <th>総損失</th>
+        <th>勝ち回数</th>
+        <th>負け回数</th>
+        <th>引き分け回数</th>
+        <th>勝率</th>
+        <th>PF</th>
+        <th>平均利益</th>
+        <th>平均損失</th>
+    </tr>
+    <tr align="right">
+        <td>{{ $pf['total_count'] }}回</td>
+        <td>{{ $pf['total_profit'] }}円</td>
+        <td>{{ $pf['win_profit'] }}円</td>
+        <td>{{ $pf['loss_profit'] }}円</td>
+        <td>{{ $pf['win_count'] }}回</td>
+        <td>{{ $pf['loss_count'] }}回</td>
+        <td>{{ $pf['draw_count'] }}回</td>
+        <td>{{ $pf['win_rate'] }}%</td>
+        <td>{{ $pf['profit_factor'] }}</td>
+        <td>{{ $pf['average_win_profit'] }}円</td>
+        <td>{{ $pf['average_loss_profit'] }}円</td>
+    </tr>
+</table>
+
+<h4>売買明細</h4>
+<button type="button" onclick="location.href='/trading-history/create'">登録</button>
+<br />
+<br />
+<table border="1" cellspacing="0" cellpadding="5">
+    <tr align="center">
+        <th>エントリー日時</th>
+        <th>決済日時</th>
+        <th>保有時間（分）</th>
+        <th>銘柄</th>
+        <th>注文タイプ</th>
+        <th>注文数</th>
+        <th>エントリー価格</th>
+        <th>決済価格</th>
+        <th>損益（円）</th>
+        <th>メモ</th>
+        <th colspan="2">操作</th>
+    </tr>
+    @foreach($trading_histories as $th)
+    <tr>
+        <td>{{ \Carbon\Carbon::create($th->entry_dateTime)->format('Y/m/d h:i') }}</td>
+        <td>{{ \Carbon\Carbon::create($th->exit_dateTime)->format('Y/m/d h:i') }}</td>
+        <td align="right">{{ \Carbon\Carbon::create($th->entry_dateTime)->diffInMinutes($th->exit_dateTime) }}</td>
+        <td align="center">{{ $symbols[$th->symbol] }}</td>
+        <td align="center">{{ $orderTypes[$th->order_type] }}</td>
+        <td align="right">{{ $th->amount }}</td>
+        <td align="right">{{ $th->entry }}</td>
+        <td align="right">{{ $th->exit }}</td>
+        <td align="right">{{ $th->profit }}</td>
+        <td>{{ $this->memo }}</td>
+        <td><button type="button" onclick="location.href='/trading-history/{{ $th->id }}/edit'">編集</button></td>
+        <td valign="middle">
+            <form action="/trading-history/{{ $th->id }}" method="POST" style="display:inline">
+                @csrf
+                @method('DELETE')
+                <button type ="submit">削除</button>
+            </form>
+        </td>
+    </tr>
+    @endforeach
+</table>
+
